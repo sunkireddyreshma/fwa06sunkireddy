d0c0f412004e3c3023f637f4ff244e1693c15990
Reshma <sunkireddyreshma1999@gmail.com>
2021-10-24 21:04:49 -0500

    values are added and commited

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
-var content = require('content');
-var router = content.Router();
+var mydata = require('mydata');
+var router = mydata.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Content'});
+  res.render('mydata', { title: 'Sunkireddy Reshmareddy'});
 });
 
 module.exports = router;
